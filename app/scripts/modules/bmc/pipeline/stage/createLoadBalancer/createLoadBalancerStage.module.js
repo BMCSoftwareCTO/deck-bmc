b802f0f01231f4b222e11c12090cd68dc4d8b47c
Viju James <viju_james@bmc.com>
2016-05-13 10:10:16 -0700

    Update createLoadBalancerStage.module.js
    Changed the order or require entries

--- app/scripts/modules/bmc/pipeline/stage/createLoadBalancer/createLoadBalancerStage.module.js
+++ app/scripts/modules/bmc/pipeline/stage/createLoadBalancer/createLoadBalancerStage.module.js
@@ -3,10 +3,10 @@
 let angular = require('angular');
 
 module.exports = angular.module('spinnaker.bmc.pipeline.stage.createLoadBalancer', [
-  require('./createLoadBalancerStage.js'),
-  require('./createLoadBalancerExecutionDetails.controller.js'),
   require('../../../../core/pipeline/config/stages/stage.module.js'),
   require('../../../../core/pipeline/config/stages/core/stage.core.module.js'),
   require('../../../../core/account/account.module.js'),
   require('../../../../core/utils/lodash.js'),
+  require('./createLoadBalancerStage.js'),
+  require('./createLoadBalancerExecutionDetails.controller.js'),
 ]);
